1f9c98563fe0ba603807535f21fe55c6d21ef91e
alibiserikbay <108667115+allebee@users.noreply.github.com>
2023-06-04 13:46:27 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import typing
+from fastapi import FastAPI, Request
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory='templates')
+
+app = FastAPI()
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
